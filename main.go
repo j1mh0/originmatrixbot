fe11e5ba7531f74b861c1dd6839342529145ee48
He Jun <>
2018-04-26 21:24:05 +0800

    修改为poll模式

--- main.go
+++ main.go
@@ -2,40 +2,28 @@ package main
 
 import (
 	"log"
-	"net/http"
 
 	"gopkg.in/telegram-bot-api.v4"
 )
 
 const (
 	botToken string = "492097173:AAHF7fNIvFe_1OFgtUy3vXXKuXuGGQ04TvM"
-	baseURL  string = "https://1e10.win:8443/"
-	port     string = "8443"
 )
 
 func main() {
 	bot, err := tgbotapi.NewBotAPI(botToken)
 	if err != nil {
-		log.Fatal(err)
+		log.Panic(err)
 	}
 
 	bot.Debug = true
 
 	log.Printf("Authorized on account %s", bot.Self.UserName)
 
-	_, err = bot.SetWebhook(tgbotapi.NewWebhookWithCert(baseURL+bot.Token, "cert.pem"))
-	if err != nil {
-		log.Fatal(err)
-	}
-	info, err := bot.GetWebhookInfo()
-	if err != nil {
-		log.Fatal(err)
-	}
-	if info.LastErrorDate != 0 {
-		log.Printf("[Telegram callback failed]%s", info.LastErrorMessage)
-	}
-	updates := bot.ListenForWebhook("/" + bot.Token)
-	go http.ListenAndServeTLS("0.0.0.0:"+port, "cert.pem", "key.pem", nil)
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates, err := bot.GetUpdatesChan(u)
 
 	for update := range updates {
 		if update.Message == nil {
